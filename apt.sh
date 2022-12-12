7f8de738003c57afbcd38741c4b8813febc57600
kartikchouhann <112246774+kartikchouhann@users.noreply.github.com>
2022-12-12 15:20:26 +0530

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-apt update -y
-apt install apache2 -y
-ufw allow ‘Apache’ -y
-systemctl restart apache2
-mkdir -p /var/www/info.net/html
-chmod -R 755 /var/www/info.net
-cp ./index.html /var/www/info.net/html
-systemctl restart apache2
+yum update -y
+yum install httpd -y
+systemctl restart httpd
+cp ./index.html /var/www/html
+systemctl restart httpd
